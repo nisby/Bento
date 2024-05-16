726cb1bbca2af306411dbf3f207fee28769b60d1
nisby <m.nista@hotmail.it>
2024-05-16 19:12:50 +0200

    Update config.js

--- config.js
+++ config.js
@@ -61,7 +61,7 @@ const CONFIG = {
 			id: '2',
 			name: 'Navidrome',
 			icon: 'disc-3',
-			link: 'https://music.nisby44.duckdns.org/app/#/album/all?sort=name&order=ASC&filter={}',
+			link: 'http://192.168.0.177:4533/app/#/album/all?sort=name&order=ASC&filter={}',
 		},
 		{
 			id: '3',
@@ -148,7 +148,7 @@ const CONFIG = {
 				},
 				{
 					name: 'Gitea',
-					link: 'https://git.nisby44.duckdns.org/matteo/myfiles',
+					link: 'http://192.168.0.177:3002/matteo/myfiles',
 				},
 				{
 					name: 'Router',
